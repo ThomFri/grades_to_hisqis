28047e861db1db9d0cd8eac047d86c20ad2d6c1e
Thom Fri <thom.fri@gmx.de>
2021-07-27 15:37:34 +0200

    scrs added

--- main.py
+++ main.py
@@ -3,6 +3,13 @@
 # Press Umschalt+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
+#https://stackoverflow.com/questions/11348347/find-non-common-elements-in-lists ==> .difference()
+
+#gui
+#https://stackoverflow.com/questions/45441885/how-can-i-create-a-dropdown-menu-from-a-list-in-tkinter
+#https://www.delftstack.com/de/howto/python-tkinter/how-to-create-dropdown-menu-in-tkinter/
+#https://docs.python.org/3/library/dialog.html
+#https://pythonbasics.org/tkinter-filedialog/
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
